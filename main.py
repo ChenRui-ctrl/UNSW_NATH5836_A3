5007200c0175e974ee2da88bff0fc7047884521d
陈锐 <ruic02351@gmail.com>
2023-11-15 16:40:06 +1100

    tree make

--- main.py
+++ main.py
@@ -13,18 +13,54 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.tree import export_graphviz
+from subprocess import call
 
 
 # Function importing Dataset
 def importdata():
-    data = pd.read_csv('C:/Users/chenr/Desktop/UNSW_NATH5836_A3/abalone.csv')
+    data = pd.read_csv('C:/Users/chenr/Desktop/UNSW_NATH5836_A3/abalone.data')
+    data = data.values
+    data[:,0] = np.where(data[:,0] == -1, 0, 1)
+    for i in range(1, data.shape[1]):
+        data = np.delete(data, data[:, i] == 0, axis=0)
+    for j in range(data.shape[0]):
+        mid = data[j, -1]
+        if mid < 8:
+            data[j, -1] = 1
+        elif mid < 11:
+            data[j, -1] = 2
+        elif mid < 16:
+            data[j, -1] = 3
+        else:
+            data[j, -1] = 4
+    data = data.astype("float")
     return data
 
-def data_convert(data):
-    data['Sex'] = data['Sex'].map({'F':1,'M':0,'I':-1})# use map()function to finish convertion
-    #print
-    return data
+def read_data():
+    abalone = pd.read_csv("C:/Users/chenr/Desktop/pythonProject/abalone.data", sep=',', header=None)
+    abalone = abalone.values
+    ### combine female and male feature as adult
+    abalone[:, 0] = np.where(abalone[:, 0] == -1, 0, 1)
+    ### clean 0 data in this dataset
+    for i in range(1, abalone.shape[1]):
+        abalone = np.delete(abalone, abalone[:, i] == 0, axis=0)
 
+    ### treat the output part by label code which means 0-7 years as 1, 8-10 years as 2, and so on
+    for j in range(abalone.shape[0]):
+        mid = abalone[j, -1]
+        if mid < 8:
+            abalone[j, -1] = 1
+        elif mid < 11:
+            abalone[j, -1] = 2
+        elif mid < 16:
+            abalone[j, -1] = 3
+        else:
+            abalone[j, -1] = 4
+    ### change to float data type
+    abalone = abalone.astype("float")
+
+    return (abalone)
 
 def autolabel(rects):
     for rect in rects:
@@ -33,11 +69,14 @@ def autolabel(rects):
 
 
 def multi_class(data, class_name):
-    data_classified = data['Rings']
+    # data_classified = data[8]
+    # df_data_classified = pd.DataFrame(data_classified)
+    df_data = pd.DataFrame(data)
+    data_classified = df_data[8]
     bins = [0,7,10,15,100]
     rings_bins = pd.cut(data_classified, bins)
     # print(rings_bins)
-    df_ring_bins = data.groupby(rings_bins)['Rings'].count()
+    df_ring_bins = df_data.groupby(rings_bins)[8].count()
     X = class_name
     a = plt.bar(X,df_ring_bins)
     plt.xlabel('Rings')
@@ -45,13 +84,15 @@ def multi_class(data, class_name):
     autolabel(a)
 
     plt.savefig('Multi Classes')
+    # data = df_data.values
+    # return data
 
 
 # Function to split the dataset
 def splitdataset(data):
     # Separating the target variable
-    X = data.values[:, :-1]
-    Y = data.values[:, -1]
+    X = data[:, :-1]
+    Y = data[:, -1]
 
     # Splitting the dataset into train and test
     X_train, X_test, y_train, y_test = train_test_split(
@@ -60,40 +101,35 @@ def splitdataset(data):
     return  X_train, X_test, y_train, y_test
 
 
-
-
-
-# Driver code
-def main():
-    # Building Phase
-    data = importdata()
-    data_converted = data_convert(data)
-
-    # print(data_convereted)
-    class_name = ['0-7', '8-10', '10-15','>15']
-    multi_class(data, class_name)
-
-    X_train, X_test, y_train, y_test = splitdataset(data)
-
-
-    from sklearn.tree import DecisionTreeClassifier
-
-    feature_name = ['Sex','Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight']
-    decision_tree = DecisionTreeClassifier(random_state=0, max_depth=10)
-    decision_tree = decision_tree.fit(X_train, y_train)
-
-
+def tree_make(X_train,y_train):
+    model = DecisionTreeClassifier(max_depth=3, criterion='gini')
+    detree = model.fit(X_train, y_train)
 
     from sklearn.tree import plot_tree
-    plt.figure(figsize=(25,10))
-    plot_tree(decision_tree,
-              feature_names=['Sex','Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight'],
-              class_names=['0-7', '8-10', '10-15','>15'],
+    plt.figure(figsize=(25, 10))
+    plot_tree(detree,
+              feature_names=['Sex', 'Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight', 'Viscera weight',
+                             'Shell weight'],
+              class_names=['0-7', '8-10', '10-15', '>15'],
               filled=True,
               rounded=True,
               fontsize=14)
 
     plt.savefig('Decision tree')
+
+
+
+# Driver code
+def main():
+    # import data
+    data = importdata()
+    # create multiiclass
+    class_name = ['0-7', '8-10', '10-15','>15']
+    multi_class(data, class_name)
+    # create decision tree
+    X_train, X_test, y_train, y_test = splitdataset(data)
+    tree_make(X_train,y_train)
+
 
 
 
